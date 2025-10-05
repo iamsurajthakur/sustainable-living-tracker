d71739bc49461ebd86ebff24f9ccd3e25627e8ed
suraj <suraj.tech.in@gmail.com>
2025-10-05 17:38:37 +0545

    setup shadcn UI library

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,11 @@
-const App = () => {
-  return <div>Frontend</div>
+import { Button } from '@/components/ui/button'
+
+function App() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
+  )
 }
 
 export default App
