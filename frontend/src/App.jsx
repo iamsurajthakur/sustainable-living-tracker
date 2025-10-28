5087fabfb9206b00b56e811a7d06ac6927e24b44
suraj <suraj.tech.in@gmail.com>
2025-10-28 14:02:30 +0545

    test more shadcn ui components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,17 @@
 import * as React from 'react'
 import { Button } from '@/components/ui/button'
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
-import { Terminal } from 'lucide-react'
+import { Terminal, ChevronDownIcon } from 'lucide-react'
 import { Calendar } from '@/components/ui/calendar'
+import { Label } from '@/components/ui/label'
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover'
 
 function App() {
+  const [open, setOpen] = React.useState(false)
   const [date, setDate] = React.useState(new Date())
 
   return (
@@ -24,7 +31,35 @@ function App() {
         selected={date}
         onSelect={setDate}
         className="rounded-lg border bg-white text-black"
+        captionLayout="dropdown"
       />
+
+      <Label htmlFor="date" className="px-1">
+        Date of birth
+      </Label>
+      <Popover open={open} onOpenChange={setOpen}>
+        <PopoverTrigger asChild>
+          <Button
+            variant="outline"
+            id="date"
+            className="w-48 justify-between font-normal"
+          >
+            {date ? date.toLocaleDateString() : 'Select date'}
+            <ChevronDownIcon />
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-auto overflow-hidden p-0" align="start">
+          <Calendar
+            mode="single"
+            selected={date}
+            captionLayout="dropdown"
+            onSelect={(date) => {
+              setDate(date)
+              setOpen(false)
+            }}
+          />
+        </PopoverContent>
+      </Popover>
     </div>
   )
 }
