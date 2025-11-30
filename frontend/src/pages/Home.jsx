3ac86e4ca263d957cafbd315e03180094aadb77f
suraj <suraj.tech.in@gmail.com>
2025-11-30 17:32:54 +0545

    add home page basic layout

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,13 @@
+import Banner from '@/components/home/Banner'
+import Hero from '@/components/home/Hero'
+
 import React from 'react'
 
 const Home = () => {
   return (
     <div>
-      <h1>Home Page</h1>
+      <Banner />
+      <Hero />
     </div>
   )
 }
