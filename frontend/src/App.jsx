8db0bf896255c619aa5b515a47a98bfcfed0303e
suraj <suraj.tech.in@gmail.com>
2025-11-30 14:25:31 +0545

    add basic pages and routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,66 +1,23 @@
+import Home from './pages/Home'
 import * as React from 'react'
-import { Button } from '@/components/ui/button'
-import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
-import { Terminal, ChevronDownIcon } from 'lucide-react'
-import { Calendar } from '@/components/ui/calendar'
-import { Label } from '@/components/ui/label'
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from '@/components/ui/popover'
+import { Route, Routes } from 'react-router-dom'
+import Layout from './pages/Layout'
+import Dashboard from './pages/Dashboard'
+import Login from './pages/Login'
 
 function App() {
-  const [open, setOpen] = React.useState(false)
-  const [date, setDate] = React.useState(new Date())
-
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center gap-6 p-6 bg-gray-900 text-white">
-      <Button>Click me</Button>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
 
-      <Alert variant="default">
-        <Terminal className="h-4 w-4" />
-        <AlertTitle>Heads up!</AlertTitle>
-        <AlertDescription>
-          You can add components and dependencies to your app using the CLI.
-        </AlertDescription>
-      </Alert>
+        <Route path="app" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+        </Route>
 
-      <Calendar
-        mode="single"
-        selected={date}
-        onSelect={setDate}
-        className="rounded-lg border bg-white text-black"
-        captionLayout="dropdown"
-      />
-
-      <Label htmlFor="date" className="px-1">
-        Date of birth
-      </Label>
-      <Popover open={open} onOpenChange={setOpen}>
-        <PopoverTrigger asChild>
-          <Button
-            variant="outline"
-            id="date"
-            className="w-48 justify-between font-normal"
-          >
-            {date ? date.toLocaleDateString() : 'Select date'}
-            <ChevronDownIcon />
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent className="w-auto overflow-hidden p-0" align="start">
-          <Calendar
-            mode="single"
-            selected={date}
-            captionLayout="dropdown"
-            onSelect={(date) => {
-              setDate(date)
-              setOpen(false)
-            }}
-          />
-        </PopoverContent>
-      </Popover>
-    </div>
+        <Route path="login" element={<Login />} />
+      </Routes>
+    </>
   )
 }
 
